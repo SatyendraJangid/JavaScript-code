780f050386bba25d55eb6c580d41592a0943d356
Satyendra Jangid <114796193+SatyendraJangid@users.noreply.github.com>
2025-03-25 09:30:33 +0000

    story of variables

---
+++ JS/variables.js
@@ -0,0 +1,18 @@
+const accountId = 1447766823
+let accountEmail = "satyendrajangid900@gmail.com"
+var accountPass = "1234"
+accountCity = "Jaipur"
+
+// accountId = 788 (Not allowed)
+accountEmail = "satyendra5364@gmail.com"
+accountPass = "4565"
+accountCity = "karauli"
+
+/*
+prefer not to use var 
+because of block scope and functional scope
+*/
+
+console.log(accountId)
+
+console.table([accountId,accountEmail,accountPass,accountCity])
