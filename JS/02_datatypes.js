c1d5b2d280f58dad5a3e961e4ceb4841ab176c22
Satyendra Jangid <114796193+SatyendraJangid@users.noreply.github.com>
2025-04-13 04:54:33 +0000

    renamed

---
+++ JS/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all js code as newer version
+
+// alert(5 + 50)  //we are using nodejs not browser
+
+console.log(4+45)
+
+console.log("Satyendar")
+
+//best code readibality
+
+//data types
+
+//number  => 2^53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined
+//symbol => to finding uniqueness
+
+
+
+//object
+
+console.log(typeof("Sattu"))
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
